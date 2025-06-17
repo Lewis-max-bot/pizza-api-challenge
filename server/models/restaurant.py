1a178d9e853ea4ba3a3e60696df25363f5792e1e
Lewis <lewis.njuma@student.moringaschool.com>
2025-06-18 00:09:59 +0300

    defined the models

---
+++ server/models/restaurant.py
@@ -0,0 +1,13 @@
+from ..app import db
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', cascade="all, delete-orphan")
+
+    def to_dict(self):
+        return {"id": self.id, "name": self.name, "address": self.address}
